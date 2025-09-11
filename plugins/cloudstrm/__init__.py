8c4c8aecf3bced3ce75182f4b7c56055a7ad53d0
shiji5824666 <64516590+shiji5824666@users.noreply.github.com>
2025-09-11 15:12:07 +0800

    Update __init__.py

--- plugins/cloudstrm/__init__.py
+++ plugins/cloudstrm/__init__.py
@@ -29,7 +29,7 @@ class CloudStrm(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/thsrite/MoviePilot-Plugins/main/icons/create.png"
     # 插件版本
-    plugin_version = "7.7.7"
+    plugin_version = "7.7.8"
     # 插件作者
     plugin_author = "thsrite"
     # 作者主页
